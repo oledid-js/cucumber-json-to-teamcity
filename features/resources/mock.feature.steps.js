9670ee80e6325691eb811ab1d614f0d0b3426871
Kevin Kuszyk <kevin@kevinkuszyk.com>
2018-01-04 14:15:43 +0000

    Upgrade to cucumber v3

--- features/resources/mock.feature.steps.js
+++ features/resources/mock.feature.steps.js
@@ -1,41 +1,40 @@
 "use strict";
 
 const expect = require("chai").expect;
+const {Before, After, Given, Then} = require("cucumber");
 
-module.exports = function () {
-	this.Before((scenario, callback) => {
-		callback();
-	});
+Before((scenario, callback) => {
+	callback();
+});
 
-	this.After((scenario, callback) => {
-		callback();
-	});
+After((scenario, callback) => {
+	callback();
+});
 
-	this.Given("something", callback => {
-		callback();
-	});
+Given("something", callback => {
+	callback();
+});
 
-	this.Then(/^the test should (.*)$/, (what, callback) => {
-		switch (what) {
-			case "pass": {
-				callback();
-				break;
-			}
-
-			case "fail": {
-				expect(true).to.equal(false);
-				callback();
-				break;
-			}
-
-			default:
-			case "throw an error": {
-				throw new Error("I am an error");
-			}
-
-			case "be ignored": {
-				callback(null, "pending");
-			}
+Then(/^the test should (.*)$/, (what, callback) => {
+	switch (what) {
+		case "pass": {
+			callback();
+			break;
 		}
-	});
-};
+
+		case "fail": {
+			expect(true).to.equal(false);
+			callback();
+			break;
+		}
+
+		default:
+		case "throw an error": {
+			throw new Error("I am an error");
+		}
+
+		case "be ignored": {
+			callback(null, "pending");
+		}
+	}
+});
